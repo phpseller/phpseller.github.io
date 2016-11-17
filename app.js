1225c1837e892f6ff783ba00f8d3dc4f6c720313
phpseller <wissembr01@gmail.com>
2016-11-17 10:48:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var config = {
   app_name: 'BTC Zen Dice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
-  redirect_uri: 'https://untitled-dice.github.io',
+  recaptcha_sitekey: '6Lf_MQwUAAAAAPWk8D_Zqlm4p4cFXXr-rD_iJtKr',  // <----- EDIT ME!
+  redirect_uri: 'https://phpseller.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: 'https://a-chat-server.herokuapp.com',
